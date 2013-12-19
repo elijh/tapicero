e4df501035434cbb1920ccca21e489599b5ad382
Azul <azul@riseup.net>
2013-12-19 13:18:28 +0100

    Version 0.2.0: use CouchRest::Changes
    This also changes the format of the config file. Please make sure to adjust it.

--- lib/tapicero.rb
+++ lib/tapicero.rb
@@ -1,28 +1,33 @@
 unless defined? BASE_DIR
   BASE_DIR = File.expand_path('../..', __FILE__)
 end
-unless defined? LEAP_CA_CONFIG
-  LEAP_CA_CONFIG = '/etc/leap/tapicero.yaml'
+unless defined? TAPICERO_CONFIG
+  TAPICERO_CONFIG = '/etc/leap/tapicero.yaml'
 end
 
 module Tapicero
   class <<self
     attr_accessor :logger
+    attr_accessor :config
+  end
+
+
+  #
+  # Load Config
+  # this must come first, because CouchRest needs the connection
+  # defined before the models are defined.
+  #
+  require 'couchrest/changes'
+  configs = ['config/default.yaml', TAPICERO_CONFIG, ARGV.grep(/\.ya?ml$/).first]
+  self.config = CouchRest::Changes::Config.load(BASE_DIR, *configs)
+  self.logger = CouchRest::Changes::Config.logger
+
+  #
+  # Load Tapicero Parts
+  #
+  require 'tapicero/user_database'
+
+  def self.user_database(id)
+    UserDatabase.new(config.couch_host, config.options[:db_prefix] + id)
   end
 end
-
-
-#
-# Load Config
-# this must come first, because CouchRest needs the connection defined before the models are defined.
-#
-require 'tapicero/config'
-Tapicero::Config.load(BASE_DIR, 'config/default.yaml', LEAP_CA_CONFIG, ARGV.grep(/\.ya?ml$/).first)
-
-#
-# Load Tapicero
-#
-# require 'tapicero/json_stream'
-# require 'tapicero/couch_stream'
-require 'tapicero/couch_changes'
-require 'tapicero/user_database'
