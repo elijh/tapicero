5d3cf83abf9123af67b3965dea7ad12022d66c3f
Azul <azul@riseup.net>
2014-02-04 09:43:24 +0100

    unify naming scheme - tapicero_test prefix

--- test/setup_couch.sh
+++ test/setup_couch.sh
@@ -6,8 +6,8 @@ curl -X GET $HOST
 echo "creating unprivileged user :"
 curl -HContent-Type:application/json -XPUT $HOST/_users/org.couchdb.user:me --data-binary '{"_id": "org.couchdb.user:me","name": "me","roles": [],"type": "user","password": "pwd"}'
 echo "creating database to watch:"
-curl -X PUT $HOST/tapicero-test-users
+curl -X PUT $HOST/tapicero_test_users
 echo "restricting database access :"
-curl -X PUT $HOST/tapicero-test-users/_security -Hcontent-type:application/json --data-binary '{"admins":{"names":[],"roles":[]},"members":{"names":["me"],"roles":[]}}'
+curl -X PUT $HOST/tapicero_test_users/_security -Hcontent-type:application/json --data-binary '{"admins":{"names":[],"roles":[]},"members":{"names":["me"],"roles":[]}}'
 echo "adding admin :"
 curl -X PUT $HOST/_config/admins/anna -d '"secret"'
