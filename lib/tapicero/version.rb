b01ee5812e920940c3515b410c7a2283e550ab26
elijah <elijah@riseup.net>
2014-09-23 16:22:19 -0700

    Don't die when couchdb gets restarted (closes #6065)

--- lib/tapicero/version.rb
+++ lib/tapicero/version.rb
@@ -1,5 +1,5 @@
 module Tapicero
-  VERSION = "0.4.0"
+  VERSION = "0.4.1"
   REQUIRE_PATHS = ['lib']
   FLAGS = []
   CONFIGS = ['config/default.yaml', '/etc/leap/tapicero.yaml']
--- lib/tapicero_daemon.rb
+++ lib/tapicero_daemon.rb
@@ -6,13 +6,16 @@
 #
 require 'tapicero'
 require 'extends/couchrest'
+require 'tapicero/user_event_handler'
 
 module Tapicero
   module Daemon
-    require 'tapicero/user_event_handler'
-    users = CouchRest::Changes.new('users')
-    UserEventHandler.new(users)
-    users.listen
-
+    while true
+      users = CouchRest::Changes.new('users')
+      UserEventHandler.new(users)
+      users.listen
+      Tapicero.logger.info('Lost contact with couchdb, will try again in 10 seconds')
+      sleep 10
+    end
   end
 end
